7a2d9a1409b12c7da574aa32f088f93f311649be
Ashish Kumar <myselfashish777@gmail.com>
2023-08-22 13:29:10 +0530

    updated

--- Multiplication App/multiplication app.js
+++ Multiplication App/multiplication app.js
@@ -1,9 +1,10 @@
-const num1 = Math.ceil(Math.random()*10);
-const num2 = Math.ceil(Math.random()*10);
+const num1 = Math.ceil(Math.random() * 10);
+const num2 = Math.ceil(Math.random() * 10);
+
 
 const questionEl = document.getElementById("question");
-const inputEl = document.getElementById("input-id");
 const formEl = document.getElementById("form-id");
+const inputEl = document.getElementById("input-id");
 const scoreEl = document.getElementById("score-id");
 
 
@@ -13,25 +14,24 @@ if(!score){
   score = 0;
 }
 
-scoreEl.innerText = `score: ${score}`;
-
-
+scoreEl.innerText = `score:${score}`;
 
 questionEl.innerText = `What is ${num1} multiply of ${num2}`;
+
 
 const correctAns = num1 * num2;
 
 formEl.addEventListener("submit", ()=>{
   const userAns = +inputEl.value;
   if(userAns === correctAns){
-    score++;
-    updateLocalStorage();
+    score++
+    updatedLocalStorage();
   } else {
-    score--;
-    updateLocalStorage();
+    score--
+    updatedLocalStorage();
   }
 });
 
-function updateLocalStorage(){
+function updatedLocalStorage(){
   localStorage.setItem("score", JSON.stringify(score));
 }
