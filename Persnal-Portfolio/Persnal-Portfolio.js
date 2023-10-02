b4c06acb7350c9f4e1a11c3116ca14acec9dbd40
Ashish Kumar <myselfashish777@gmail.com>
2023-10-02 12:54:02 +0530

    updated

--- Persnal-Portfolio/Persnal-Portfolio.js
+++ Persnal-Portfolio/Persnal-Portfolio.js
@@ -15,3 +15,32 @@ function opentab(tabname){
   event.currentTarget.classList.add("active-link");
   document.getElementById(tabname).classList.add("active-tab");
 }
+
+// side menu code
+let sidemenu = document.querySelector("#sidemenu");
+
+function openmenu(){
+  sidemenu.style.right = "0";
+}
+
+function closemenu(){
+  sidemenu.style.right = "-200px";
+}
+
+
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbyXuHTFmBtnfA8KCXZneMzCSqc4MUqM87tzkD7xi9VcjyUxxg88tFj_OodBQ-_7Ctmi/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const msg = document.getElementById("msg");
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML = "Message sent successfully!"
+        setTimeout(function(){
+          innerHTML = ""
+        }, 5000);
+        form.reset()
+    })
+      .catch(error => console.error('Error!', error.message))
+  })
